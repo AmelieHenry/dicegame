9f502525ee8bd41b06f1187a6281955965a5e0d0
AmelieBigras <bigrasamelie90@gmail.com>
2022-08-09 12:48:14 -0600

    first push

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*7);
+
+var newDice = "dice" + randomNumber1 + ".png";
+
+var newSrc = "/img" + newDice;
+
+document.querySelector(img1)[0].setAttribute("src", newSrc);
+
+var randomNumber2 = Math.floor(Math.random()*7);
+
+var newDice2 = "dice" + randomNumber2 + ".png";
+
+var newSrc2 = "/img" + newDice2;
+
+document.querySelector("img")[1].setAttribute("src", newSrc2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").setAttribute(h1, "🚩Player 1 wins!")
+};
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").setAttribute(h1, "Player 2 wins!🚩")
+};
+
+else (randomNumber1 = randomNumber2) {
+    document.querySelector("h1").setAttribute(h1, "🚩Draw!🚩")
+};
